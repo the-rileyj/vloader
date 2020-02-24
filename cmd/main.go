36f085ed6fe4ed492e2a3aed48ee31767c79d1af
Riley <rileyjohnson99@hotmail.com>
2020-02-23 23:06:11 -0600

    working on streaming

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,26 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"os/exec"
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
+	"time"
 
-	"github.com/atotto/clipboard"
+	"github.com/gin-gonic/gin"
+	"github.com/tcolgate/mp3"
 )
 
 // ===============
@@ -342,7 +349,7 @@ func newYouTube(URL string) (*YouTube, error) {
 
 		err = json.Unmarshal(embedYTPlayerConfigMatch[1], &youtubeVideoInfo)
 
-		clipboard.WriteAll(videoInfoValues.Get("player_response"))
+		// clipboard.WriteAll(videoInfoValues.Get("player_response"))
 	} else {
 		videoInfoMatch := YTPlayerConfigRegex.FindSubmatch(watchHTMLBytes)
 
@@ -562,14 +569,18 @@ var audioExtensionMap = map[string]string{
 	"mp4": "m4a",
 }
 
-func main() {
-	youtube, err := newYouTube("https://www.youtube.com/watch?v=DBzuYNK95sM")
+func convertFileToMP3(trackReader io.Reader, outputWriter io.Writer) error {
+	ffmpegCommand := exec.Command("ffmpeg", "-i", "-", "-f", "mp3", "-vn", "-ar", "44100", "-ac", "2", "-b:a", "192k", "-f", "mp3", "-")
 
-	if err != nil {
-		panic(err)
-	}
+	ffmpegCommand.Stdin = trackReader
+	ffmpegCommand.Stdout = outputWriter
+	ffmpegCommand.Stderr = os.Stdout
 
-	for _, stream := range youtube.Streams {
+	return ffmpegCommand.Run()
+}
+
+func getYoutubeVideoBytes(youtubeVideo *YouTube) ([]byte, error) {
+	for _, stream := range youtubeVideo.Streams {
 		if strings.HasPrefix(stream.Type, "audio/mp4") {
 			videoDownloadRequest, err := youtubeGetRequest(stream.URL)
 
@@ -583,19 +594,344 @@ func main() {
 				panic(err)
 			}
 
-			downloadFile, err := os.Create("nice.mp4")
+			trackBuffer := &bytes.Buffer{}
 
-			io.Copy(downloadFile, videoDownloadResponse.Body)
+			err = convertFileToMP3(videoDownloadResponse.Body, trackBuffer)
 
-			videoDownloadResponse.Body.Close()
+			if err != nil {
+				return nil, err
+			}
 
-			downloadFile.Close()
+			return trackBuffer.Bytes(), nil
 		}
 	}
+
+	return nil, errors.New("could not find m4a stream for video")
+}
+
+func main() {
+	streamerPlayerlist, err := NewStreamerPlaylist([]string{
+		"https://www.youtube.com/watch?v=w__wUYxMjYE",
+		"https://www.youtube.com/watch?v=FVPmwbahDWM",
+		"https://www.youtube.com/watch?v=T8Zj1oLGaQE",
+		// "https://www.youtube.com/watch?v=mdu5lLpMH_w",
+		// This errors out because finding the stream signature doesn't work
+		// "https://www.youtube.com/watch?v=TxXG0k2QpqI",
+		"https://www.youtube.com/watch?v=DBzuYNK95sM",
+		"https://www.youtube.com/watch?v=Fl-e2PM1ITM",
+		"https://www.youtube.com/watch?v=yCBa5-g_Ibw",
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	// downloadFile, err := os.Create("nice.mp4")
+
+	// io.Copy(downloadFile, videoDownloadResponse.Body)
+
+	// videoDownloadResponse.Body.Close()
+
+	// downloadFile.Close()
+
+	router := gin.Default()
+
+	s := streamer{}
+
+	buffSize := 10
+	readSize := 1
+	writeBuff := 32768
+	queueSize := 10
+
+	s.Input = streamerPlayerlist
+	// NewLooper(trackBytes)
+	s.ReadSize = time.Duration(readSize) * time.Second
+	s.BuffSize = time.Duration(buffSize) * time.Second
+	s.QueueSize = queueSize
+	s.WriteBuff = writeBuff
+	err = s.init()
+
+	go s.readLoop()
+
+	router.GET("/live", func(c *gin.Context) {
+		id, recieve := s.addClient()
+		defer s.delClient(id)
+
+		c.Header("Content-Type", "audio/mpeg")
+		c.Header("Connection", "Keep-Alive")
+		c.Header("Cache-Control", "no-cache")
+
+		head := []byte{0x49, 0x44, 0x33, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+
+		buffw := bufio.NewWriterSize(c.Writer, s.WriteBuff)
+		if _, err := buffw.Write(head); err != nil {
+			return
+		}
+
+		//Copy buffer
+		s.RLock()
+
+		buf := make([]byte, len(s.buffer))
+		copy(buf, s.buffer)
+
+		s.RUnlock()
+		if _, err := buffw.Write(buf); err != nil {
+			return
+		}
+		buf = nil
+
+		for {
+			chunk := <-recieve
+
+			if _, err := buffw.Write(chunk); err != nil {
+				return
+			}
+		}
+	})
+
+	panic(router.Run(":8987"))
 
 	// _, err = newYouTube("https://www.youtube.com/watch?v=Fl-e2PM1ITM&bpctr=1581197742")
 
 	// if err != nil {
 	// 	panic(err)
 	// }
+}
+
+// Looper
+
+type Loop struct {
+	Data    []byte
+	pointer int
+	length  int
+}
+
+func NewLooper(data []byte) *Loop {
+	return &Loop{
+		Data:    data,
+		pointer: 0,
+		length:  len(data),
+	}
+}
+
+func (l *Loop) Reset() { l.pointer = 0 }
+
+// return bytes as long as you are asked for it
+func (l *Loop) Read(p []byte) (n int, err error) {
+	for i := 0; i < len(p); i++ {
+		dataPos := (l.pointer + i) % l.length
+		p[i] = l.Data[dataPos]
+	}
+	l.pointer = (l.pointer + len(p)) % l.length
+	return len(p), nil
+}
+
+// StreamerList
+
+type SongInfo struct {
+	Name   string `json:"name"`
+	Length int    `json:"length"`
+	URL    string `json:"URL"`
+}
+
+type StreamerPlaylist struct {
+	PlaylistInfo  []SongInfo
+	Data          []byte
+	currentLength int
+	currentIndex  int
+	pointer       int
+	nexting       bool
+	streamLock    *sync.Mutex
+}
+
+func NewStreamerPlaylist(youtubeURLs []string) (*StreamerPlaylist, error) {
+	startCount := 2
+
+	sp := &StreamerPlaylist{currentIndex: startCount, Data: make([]byte, 0), PlaylistInfo: make([]SongInfo, 0), streamLock: &sync.Mutex{}}
+
+	for _, youtubeURL := range youtubeURLs {
+		songInfo := SongInfo{URL: youtubeURL}
+
+		if len(sp.PlaylistInfo) < startCount+1 {
+			youtubeVideo, err := newYouTube(youtubeURL)
+
+			if err != nil {
+				return nil, err
+			}
+
+			youtubeVideoBytes, err := getYoutubeVideoBytes(youtubeVideo)
+
+			if err != nil {
+				return nil, err
+			}
+
+			songInfo.Length = len(youtubeVideoBytes)
+
+			sp.Data = append(sp.Data, youtubeVideoBytes...)
+		}
+
+		sp.PlaylistInfo = append(sp.PlaylistInfo, songInfo)
+	}
+
+	sp.currentLength = sp.PlaylistInfo[0].Length
+
+	return sp, nil
+}
+
+func (sp *StreamerPlaylist) Next() {
+	fmt.Println("\nnexting\n")
+
+	sp.currentIndex++
+
+	youtubeVideo, err := newYouTube(sp.PlaylistInfo[sp.currentIndex].URL)
+
+	if err != nil {
+		panic(err)
+	}
+
+	youtubeVideoBytes, err := getYoutubeVideoBytes(youtubeVideo)
+
+	sp.PlaylistInfo[sp.currentIndex].Length = len(youtubeVideoBytes)
+
+	if err != nil {
+		panic(err)
+	}
+
+	sp.streamLock.Lock()
+
+	fmt.Println(sp.PlaylistInfo[sp.currentIndex-3].URL, sp.PlaylistInfo[sp.currentIndex-2].URL)
+
+	sp.Data = append(sp.Data[:sp.currentLength], youtubeVideoBytes...)
+	sp.pointer = sp.pointer % sp.currentLength
+	sp.currentLength = sp.PlaylistInfo[sp.currentIndex-2].Length
+	sp.nexting = false
+
+	sp.streamLock.Unlock()
+}
+
+// return bytes as long as you are asked for it
+func (l *StreamerPlaylist) Read(p []byte) (n int, err error) {
+	l.streamLock.Lock()
+
+	defer l.streamLock.Unlock()
+
+	for i := 0; i < len(p); i++ {
+		p[i] = l.Data[l.pointer+i]
+	}
+
+	l.pointer += len(p)
+
+	if l.pointer > l.currentLength && !l.nexting {
+		l.nexting = true
+
+		go l.Next()
+	}
+
+	return len(p), nil
+}
+
+// Streamer
+
+type streamer struct {
+	sync.RWMutex
+	clients   map[uint64]chan []byte
+	id        uint64
+	buffer    []byte
+	BuffSize  time.Duration
+	ReadSize  time.Duration
+	QueueSize int
+	WriteBuff int
+	Input     io.Reader
+	dec       *mp3.Decoder
+	frame     *mp3.Frame
+	skipped   *int
+	Stop      chan bool
+}
+
+func (s *streamer) init() (err error) {
+	s.Lock()
+	defer s.Unlock()
+	s.frame = new(mp3.Frame)
+	s.skipped = new(int)
+	s.clients = make(map[uint64]chan []byte)
+	s.dec = mp3.NewDecoder(s.Input)
+	s.buffer, _, err = s.readChunk(s.BuffSize)
+	s.Stop = make(chan bool)
+	if err != nil {
+		return
+	}
+	log.Println("Buffer created...")
+	return
+}
+
+func (s *streamer) readLoop() {
+	defer close(s.Stop)
+	var wait time.Duration
+	var start time.Time
+	for {
+		start = time.Now()
+		buf, dur, err := s.readChunk(s.ReadSize)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		s.send(buf)
+		s.Lock()
+		if len(s.buffer) < len(buf) {
+			s.buffer = buf
+		} else {
+			s.buffer = append(s.buffer[len(buf):], buf...)
+		}
+		s.Unlock()
+		wait += dur - time.Now().Sub(start)
+		if wait > dur {
+			time.Sleep(wait)
+			wait = 0
+		}
+	}
+}
+
+func (s *streamer) addClient() (uint64, chan []byte) {
+	s.Lock()
+	defer s.Unlock()
+	s.id++
+	s.clients[s.id] = make(chan []byte, s.QueueSize)
+	return s.id, s.clients[s.id]
+}
+
+func (s *streamer) delClient(id uint64) {
+	s.Lock()
+	defer s.Unlock()
+	close(s.clients[id])
+	delete(s.clients, id)
+}
+
+func (s *streamer) send(b []byte) {
+	s.RLock()
+	defer s.RUnlock()
+	for _, v := range s.clients {
+		select {
+		case v <- b:
+		default:
+		}
+	}
+}
+
+func (s *streamer) readChunk(expd time.Duration) (buf []byte, reald time.Duration, err error) {
+	for {
+		err = s.dec.Decode(s.frame, s.skipped)
+		if err != nil {
+			return
+		}
+		var tmp []byte
+		tmp, err = ioutil.ReadAll(s.frame.Reader())
+		if err != nil {
+			return
+		}
+		buf = append(buf, tmp...)
+		reald += s.frame.Duration()
+		if expd < reald {
+			return
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module rjstream
 
 go 1.13
 
-require github.com/atotto/clipboard v0.1.2
+require (
+	github.com/atotto/clipboard v0.1.2 // indirect
+	github.com/gin-gonic/gin v1.5.0
+	github.com/hajimehoshi/go-mp3 v0.2.1 // indirect
+	github.com/tcolgate/mp3 v0.0.0-20170426193717-e79c5a46d300
+)
